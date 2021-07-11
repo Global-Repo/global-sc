a27a97be6fbbdaa933fbe84096dc48f01f95c207
duhd4h <duhd4h@gmail.com>
2021-07-11 20:39:26 +0200

    Tests for native token and second mint function removed from contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    const NativeToken = await hre.ethers.getContractFactory("NativeToken");
+    const nativeToken = await NativeToken.deploy();
+    await nativeToken.deployed();
 
-  // We get the contract to deploy
-  const SafeMath = await hre.ethers.getContractFactory("SafeMath");
-  const safeMath = await SafeMath.deploy();
-  await safeMath.deployed();
-
-  console.log("SafeMath deployed to:", safeMath.address);
+    console.log("NativeToken deployed to:", nativeToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
