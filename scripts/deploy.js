984fc63edb35282369b046b9ba28b770da77eb2f
duhd4h <duhd4h@gmail.com>
2021-07-13 21:09:24 +0200

    Added Factory and Router to deploy.js (missing weth address and token)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,6 +9,8 @@ const DEV_ADDRESS = "0xae1671Faa94A7Cc296D3cb0c3619e35600de384C";
 const NATIVE_TOKEN_PER_BLOCK = BigNumber.from(40).mul(BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER);
 
 async function main() {
+    [owner, addr1, addr2, ...addrs] = await hre.ethers.getSigners();
+
     const CURRENT_BLOCK = await ethers.provider.getBlockNumber();
     console.log("Current block is:", CURRENT_BLOCK);
 
@@ -16,9 +18,13 @@ async function main() {
     const nativeToken = await NativeToken.deploy();
     await nativeToken.deployed();
 
+    console.log("NativeToken deployed to:", nativeToken.address);
+
     // const QuoteToken = await hre.ethers.getContractFactory("QuoteToken");
     // const quoteToken = await QuoteToken.deploy(nativeToken.address);
     // await quoteToken.deployed();
+
+    // console.log("QuoteToken deployed to:", quoteToken.address);
 
     const MasterChef = await hre.ethers.getContractFactory("MasterChef");
     const masterChef = await MasterChef.deploy(
@@ -29,11 +35,22 @@ async function main() {
     );
     await masterChef.deployed();
 
-    console.log("NativeToken deployed to:", nativeToken.address);
-    // console.log("QuoteToken deployed to:", quoteToken.address);
     console.log("Masterchef deployed to:", masterChef.address);
 
     // TODO: mint x tokens and change token owner by masterchef address
+
+    const Factory = await hre.ethers.getContractFactory("Factory");
+    const factory = await Factory.deploy(owner.address);
+    await factory.deployed();
+
+    console.log("Factory deployed to:", factory.address);
+
+    const WETH_ADDRESS = nativeToken.address; // TODO: create weth for local
+    const Router = await hre.ethers.getContractFactory("Router");
+    const router = await Router.deploy(factory.address, WETH_ADDRESS);
+    await router.deployed();
+
+    console.log("Router deployed to:", router.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
