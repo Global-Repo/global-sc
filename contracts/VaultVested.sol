6a18780b63e94667c180a76d6122908c09d5024f
duhd4h <duhd4h@gmail.com>
2021-08-17 22:47:14 +0200

    vault vested code cleaned

--- contracts/VaultVested.sol
+++ contracts/VaultVested.sol
@@ -3,16 +3,10 @@ pragma solidity ^0.6.12;
 
 import "./SafeBEP20.sol";
 import "./Math.sol";
-import "./IStrategy.sol";
 import "./IGlobalMasterChef.sol";
-import "./PausableUpgradeable.sol";
-import "./WhitelistUpgradeable.sol";
-import "./IMinter.sol";
-import "./IRouterV2.sol";
-import "./TokenAddresses.sol";
-import './IPathFinder.sol';
+import './DepositoryRestriction.sol';
 
-contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
+contract VaultVested is DepositoryRestriction {
     using SafeBEP20 for IBEP20;
     using SafeMath for uint;
     using SafeMath for uint16;
@@ -20,18 +14,13 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
     IBEP20 private global;
     IBEP20 private bnb;
     IGlobalMasterChef private globalMasterChef;
-    IMinter private minter;
-    address private treasury;
     address private vaultLocked;
-    IRouterV2 private router;
-    IPathFinder private pathFinder;
-    TokenAddresses private tokenAddresses;
 
     uint private constant DUST = 1000;
-    address private constant GLOBAL_BURN_ADDRESS = 0x000000000000000000000000000000000000dEaD;
 
     uint256 public pid;
     uint public totalShares;
+    uint public minTokenAmountToDistribute;
     mapping (address => uint) private _shares;
     mapping (address => uint) private _principal;
     mapping (address => uint) private _depositedAt;
@@ -44,12 +33,9 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
 
     PenaltyFees public penaltyFees;
 
-    uint public minTokenAmountToDistribute;
-
     event Deposited(address indexed user, uint amount);
     event Withdrawn(address indexed user, uint amount, uint withdrawalFee);
     event ProfitPaid(address indexed user, uint amount);
-    event Harvested(uint profit);
     event Recovered(address token, uint amount);
     event Distributed(uint distributedAmount, uint numberOfUsers);
 
@@ -62,47 +48,17 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
         address _global,
         address _bnb,
         address _globalMasterChef,
-        address _treasury,
-        address _vaultLocked,
-        address _tokenAddresses,
-        address _router,
-        address _pathFinder
+        address _vaultLocked
     ) public {
         pid = 0;
         global = IBEP20(_global);
         bnb = IBEP20(_bnb);
         globalMasterChef = IGlobalMasterChef(_globalMasterChef);
-        treasury = _treasury;
         vaultLocked = _vaultLocked;
 
-        global.safeApprove(_globalMasterChef, uint(0));
-        global.safeApprove(_globalMasterChef, uint(~0));
-
-        __PausableUpgradeable_init();
-        __WhitelistUpgradeable_init();
-
-        tokenAddresses = TokenAddresses(_tokenAddresses);
-        router = IRouterV2(_router);
-        pathFinder = IPathFinder(_pathFinder);
+        _allowance(global, _globalMasterChef);
 
         minTokenAmountToDistribute = 1e18; // 1 BEP20 Token
-    }
-
-    // init minter
-    function setMinter(address _minter) external {
-        require(IMinter(_minter).isMinter(address(this)) == true, "This vault must be a minter in minter's contract");
-        global.safeApprove(_minter, 0);
-        global.safeApprove(_minter, uint(~0));
-        minter = IMinter(_minter);
-    }
-
-    function canMint() internal view returns (bool) {
-        return address(minter) != address(0) && minter.isMinter(address(this));
-    }
-
-    // TODO: remove it, used for test only
-    function isVaultMintable() external view returns (bool) {
-        return address(minter) != address(0) && minter.isMinter(address(this));
     }
 
     function totalSupply() external view returns (uint) {
@@ -148,21 +104,20 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
     }
 
     function rewardsToken() external view returns (address) {
-        return address(global);
+        return address(bnb);
     }
 
-    // TODO: from variable perque sigui del user i no del vault X que els envia
-    function deposit(uint _amount, address _account) public {
-        _deposit(_amount, _account);
+    function deposit(uint _amount, address _account) public onlyDepositories {
+        global.safeTransferFrom(msg.sender, address(this), _amount);
 
-        if (isWhitelist(msg.sender) == false) {
-            _principal[msg.sender] = _principal[msg.sender].add(_amount);
-            _depositedAt[msg.sender] = block.timestamp;
-        }
-    }
+        uint shares = totalShares == 0 ? _amount : (_amount.mul(totalShares)).div(balance());
+        totalShares = totalShares.add(shares);
+        _shares[_account] = _shares[_account].add(shares);
+        _principal[msg.sender] = _principal[msg.sender].add(_amount);
+        _depositedAt[msg.sender] = block.timestamp;
 
-    function depositAll(address _account) external {
-        deposit(global.balanceOf(_account), _account);
+        uint globalHarvested = _depositStakingToken(_amount);
+        emit Deposited(msg.sender, _amount);
     }
 
     function withdrawAll() external distributeTokens {
@@ -180,24 +135,6 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
         delete _principal[msg.sender];
         delete _depositedAt[msg.sender];
         delete _bnbEarned[msg.sender];
-
-        _harvest(globalHarvested);
-    }
-
-    function harvest() external {
-        uint globalHarvested = _withdrawStakingToken(0);
-        _harvest(globalHarvested);
-    }
-
-    function withdrawShares(uint shares) external distributeTokens onlyWhitelisted {
-        uint amount = balance().mul(shares).div(totalShares);
-
-        uint globalHarvested = _withdrawStakingToken(amount);
-
-        totalShares = totalShares.sub(shares);
-        _shares[msg.sender] = _shares[msg.sender].sub(shares);
-
-        _harvest(globalHarvested);
     }
 
     function withdrawUnderlying(uint _amount) external distributeTokens {
@@ -209,8 +146,17 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
         totalShares = totalShares.sub(shares);
         _shares[msg.sender] = _shares[msg.sender].sub(shares);
         _principal[msg.sender] = _principal[msg.sender].sub(amount);
+    }
 
-        _harvest(globalHarvested);
+    function getReward() external distributeTokens {
+        uint amount = earned(msg.sender);
+        uint shares = Math.min(amount.mul(totalShares).div(balance()), _shares[msg.sender]);
+
+        uint globalHarvested = _withdrawStakingToken(amount);
+
+        totalShares = totalShares.sub(shares);
+        _shares[msg.sender] = _shares[msg.sender].sub(shares);
+        _cleanupIfDustShares();
     }
 
     function handlePenaltyFees(uint _amount) private {
@@ -246,19 +192,6 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
         emit ProfitPaid(msg.sender, _bnbEarned[msg.sender]);
     }
 
-    function getReward() external {
-        uint amount = earned(msg.sender);
-        uint shares = Math.min(amount.mul(totalShares).div(balance()), _shares[msg.sender]);
-
-        uint globalHarvested = _withdrawStakingToken(amount);
-
-        totalShares = totalShares.sub(shares);
-        _shares[msg.sender] = _shares[msg.sender].sub(shares);
-        _cleanupIfDustShares();
-
-        _harvest(globalHarvested);
-    }
-
     function _depositStakingToken(uint amount) private returns(uint globalHarvested) {
         uint before = global.balanceOf(address(this));
         globalMasterChef.enterStaking(amount);
@@ -271,32 +204,17 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
         globalHarvested = global.balanceOf(address(this)).sub(amount).sub(before);
     }
 
-    function _harvest(uint globalAmount) private {
-        if (globalAmount > 0) {
-            emit Harvested(globalAmount);
-            globalMasterChef.enterStaking(globalAmount);
-        }
-    }
-
-    function _deposit(uint _amount, address _to) private notPaused {
-        global.safeTransferFrom(msg.sender, address(this), _amount);
-
-        uint shares = totalShares == 0 ? _amount : (_amount.mul(totalShares)).div(balance());
-        totalShares = totalShares.add(shares);
-        _shares[_to] = _shares[_to].add(shares);
-
-        uint globalHarvested = _depositStakingToken(_amount);
-        emit Deposited(msg.sender, _amount);
-
-        _harvest(globalHarvested);
-    }
-
     function _cleanupIfDustShares() private {
         uint shares = _shares[msg.sender];
         if (shares > 0 && shares < DUST) {
             totalShares = totalShares.sub(shares);
             delete _shares[msg.sender];
         }
+    }
+
+    function _allowance(IBEP20 _token, address _account) private {
+        _token.safeApprove(_account, uint(0));
+        _token.safeApprove(_account, uint(~0));
     }
 
     function _distribute() private {
@@ -316,13 +234,5 @@ contract VaultVested is PausableUpgradeable, WhitelistUpgradeable {
 
         emit Distributed(currentBNBAmount, _shares.length);
         */
-    }
-
-    // SALVAGE PURPOSE ONLY
-    // @dev _stakingToken(GLOBAL) must not remain balance in this contract. So dev should be able to salvage staking token transferred by mistake.
-    function recoverToken(address _token, uint amount) virtual external onlyOwner {
-        IBEP20(_token).safeTransfer(owner(), amount);
-
-        emit Recovered(_token, amount);
     }
 }
