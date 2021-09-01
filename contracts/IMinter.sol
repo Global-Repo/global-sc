cdd081d37e213027b3cad41e5d8f8166446a1d8f
BOJI <jordi.radev@gmail.com>
2021-09-02 01:47:16 +0200

    mintNativeTokens ja no retorna el nativeTokenLockedVaultAddr

--- contracts/IMinter.sol
+++ contracts/IMinter.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.6.12;
 interface IMinter {
     function isMinter(address account) view external returns (bool);
     function setMinter(address minter, bool canMint) external;
-    function mintNativeTokens(uint, address) external returns (address);
+    function mintNativeTokens(uint, address) external;
 }
--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -291,7 +291,7 @@ contract MasterChef is Ownable, DevPower, ReentrancyGuard, IMinter, Trusted {
 
     // La funció de mintfor al nostre MC només requerirà saber quants tokens MINTEJEM i li enviem al vualt, ja que les fees son independents de cada pool i es tractaran individualment.
     // Per lo tant, els càlculs de quants tokens volem, sempre es faràn al propi vault. La lògica queda delegada al vault.
-    function mintNativeTokens(uint _quantityToMint, address userFor) external override onlyMinter returns (address) {
+    function mintNativeTokens(uint _quantityToMint, address userFor) external override onlyMinter {
         // Mintem un ~10% dels tokens a l'equip (10/110)
         nativeToken.mints(devAddr, _quantityToMint.div(10));
 
@@ -302,8 +302,6 @@ contract MasterChef is Ownable, DevPower, ReentrancyGuard, IMinter, Trusted {
         {
             mintNotifier.notify(msg.sender, userFor, _quantityToMint);
         }
-
-        return nativeTokenLockedVaultAddr;
     }
 
     // Quantes pools tenim en marxa?
