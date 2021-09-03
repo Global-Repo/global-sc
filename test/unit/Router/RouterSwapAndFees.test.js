68324b8067869b655b69143f4ef7f8b20dfbbed2
xfold <xfaran@hotmail.com>
2021-09-03 01:03:26 +0100

    swap commit router

--- test/unit/Router/RouterSwapAndFees.test.js
+++ test/unit/Router/RouterSwapAndFees.test.js
@@ -333,7 +333,8 @@ describe("Swap tokens", function () {
         console.log('owner_pair_balance', owner_pair_balance.toString());
 
         console.log("\nOwner starts swapping 3x times 100000 ntv->B, 100000 B->ntv")
-        //TODO aquest es el test, si el swap de
+        //TODO aquest es el test, forço un swap per a mirar les fees generades.
+        //TODO per a comprovar valors exactes, deixar nomes un swap de 10000 (no loop, no doble swap)
         for(let i = 0; i < 3; i++) {
             await router.connect(owner).swapExactTokensForTokens(
                 10000,
