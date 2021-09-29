ee52fda9ee58a73348b59e1d642f5ed47cf18c95
Jordi Radev <>
2021-09-30 01:57:13 +0200

    PresaleDeploy + PresaleTest

---
+++ scripts/deploy_presale_only.js
@@ -0,0 +1,57 @@
+const hre = require("hardhat");
+const { deployMasterChef } = require("../test/helpers/singleDeploys.js");
+const { BigNumber } = require("@ethersproject/bignumber");
+require("@nomiclabs/hardhat-ethers");
+const {ethers} = require("hardhat");
+
+const TOKEN_DECIMALS = 18;
+const BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER = BigNumber.from(10).pow(TOKEN_DECIMALS);
+
+// Setup
+let feeSetterAddress = null;
+let masterChefStartBlock = null;
+const NATIVE_TOKEN_PER_BLOCK = BigNumber.from(40).mul(BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER);
+
+// Deployed contracts
+let masterChef;
+let masterChefInternal;
+
+async function main() {
+    [owner, ...addrs] = await hre.ethers.getSigners();
+
+    const CURRENT_BLOCK = await ethers.provider.getBlockNumber();
+    console.log("Current block is:", CURRENT_BLOCK);
+
+    // Setup
+    feeSetterAddress = owner.address;
+    masterChefStartBlock = CURRENT_BLOCK + 1;
+
+    const MasterChefInternal = await ethers.getContractFactory("MasterChefInternal");
+    masterChefInternal = await MasterChefInternal.deploy("0xD190C873C875F4DD85D7AeD8CCddAB11cC88C485");
+    await masterChefInternal.deployed();
+    console.log("Masterchef Internal deployed to:", masterChefInternal.address);
+
+    const MasterChef = await ethers.getContractFactory("MasterChef");
+    masterChef = await MasterChef.deploy(
+        masterChefInternal.address,
+        "0x6fA19aEBd7BEF3D7e351532A69908d33b57E5fDE",
+        NATIVE_TOKEN_PER_BLOCK,
+        masterChefStartBlock,
+        "0x793793C732645eA7506dc52387C7d38A6804f303",
+        "0xD190C873C875F4DD85D7AeD8CCddAB11cC88C485",
+        "0x64787D2F505A006907A160f76e24Ed732fc6FDA6"
+    );
+    await masterChef.deployed();
+    console.log("Masterchef deployed to:", masterChef.address);
+    console.log("Globals per block: ", NATIVE_TOKEN_PER_BLOCK.toString());
+    console.log("Start block", masterChefStartBlock);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ test/unit/Utilities/PresaleDeploy.test.js
@@ -0,0 +1,92 @@
+const ethers = require("hardhat").ethers;
+const { expect } = require("chai");
+const { BigNumber } = require("@ethersproject/bignumber");
+
+const TOKEN_DECIMALS = 18;
+const BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER = BigNumber.from(10).pow(TOKEN_DECIMALS);
+const NATIVE_TOKEN_PER_BLOCK = BigNumber.from(40).mul(BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER);
+const DAY_IN_SECONDS = 86400;
+
+let startBlock = null;
+
+let nativeToken;
+let factory;
+let router;
+let tokenA;
+let tokenB;
+let weth;
+let masterChef;
+let masterChefInternal;
+
+beforeEach(async function () {
+  [owner, addr1, lockedVault, ...addrs] = await ethers.getSigners();
+
+  const CURRENT_BLOCK = await ethers.provider.getBlockNumber();
+  startBlock = CURRENT_BLOCK + 1;
+
+  const NativeToken = await ethers.getContractFactory("NativeToken");
+  nativeToken = await NativeToken.deploy();
+  await nativeToken.deployed();
+
+  const TokenA = await ethers.getContractFactory("BEP20");
+  tokenA = await TokenA.deploy('tokenA', 'AA');
+  await tokenA.deployed();
+
+  const TokenB = await ethers.getContractFactory("BEP20");
+  tokenB = await TokenB.deploy('tokenB', 'BB');
+  await tokenB.deployed();
+
+  const Factory = await ethers.getContractFactory("Factory");
+  factory = await Factory.deploy(owner.address);
+  await factory.deployed();
+
+  // TODO: should be same contract as mainet or BEP20 is okay?
+  // TODO: https://bscscan.com/address/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c#code
+  const Weth = await ethers.getContractFactory("BEP20");
+  weth = await Weth.deploy('Wrapped BNB', 'WBNB');
+  await weth.deployed();
+
+  const Router = await ethers.getContractFactory("Router");
+  router = await Router.deploy(factory.address, weth.address);
+  await router.deployed();
+
+  const TokenAddresses = await ethers.getContractFactory("TokenAddresses");
+  tokenAddresses = await TokenAddresses.deploy();
+  await tokenAddresses.deployed();
+
+  const PathFinder = await ethers.getContractFactory("PathFinder");
+  pathFinder = await PathFinder.deploy(tokenAddresses.address);
+  await pathFinder.deployed();
+
+  const MasterChefInternal = await ethers.getContractFactory("MasterChefInternal");
+  masterChefInternal = await MasterChefInternal.deploy(tokenAddresses.address);
+  await masterChefInternal.deployed();
+
+  const MasterChef = await ethers.getContractFactory("MasterChef");
+  masterChef = await MasterChef.deploy(
+      masterChefInternal.address,
+      nativeToken.address,
+      NATIVE_TOKEN_PER_BLOCK,
+      startBlock,
+      router.address,
+      tokenAddresses.address,
+      pathFinder.address
+  );
+  await masterChef.deployed();
+
+  await pathFinder.transferOwnership(masterChef.address);
+
+  // Set up scenarios
+  const INITIAL_SUPPLY = BigNumber.from(100).mul(BIG_NUMBER_TOKEN_DECIMALS_MULTIPLIER);
+
+  await tokenA.mint(INITIAL_SUPPLY);
+  await tokenB.mint(INITIAL_SUPPLY);
+  await tokenA.approve(router.address, INITIAL_SUPPLY.toHexString());
+  await tokenB.approve(router.address, INITIAL_SUPPLY.toHexString());
+});
+
+describe("MasterChef: After deployment", function () {
+  it("Should have 1 pool", async function () {
+    expect(await masterChef.poolLength()).to.equal(1);
+  });
+});
