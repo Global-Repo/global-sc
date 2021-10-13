01dd106f7bbdb8d123404d6063f05d2185053920
duhd4h <duhd4h@gmail.com>
2021-10-14 01:07:32 +0200

    TAH-01 Missing access restriction fixed

--- contracts/Helpers/TokenAddresses.sol
+++ contracts/Helpers/TokenAddresses.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-contract TokenAddresses {
+import "../Modifiers/Ownable.sol";
+
+contract TokenAddresses is Ownable {
     string public constant GLOBAL = "GLOBAL";
     string public constant CAKE = "CAKE";
-    string public constant BNB = "BNB";   // ERC20 on eth
-    string public constant WBNB = "WBNB"; // BEP20 on bsc
+    string public constant BNB = "BNB";
+    string public constant WBNB = "WBNB";
     string public constant BUSD = "BUSD";
-    string public constant BUNNY = "BUNNY";
     string public constant CAKE_WBNB_LP = "CAKE-WBNB-LP";
 
     mapping (string => address) private tokens;
@@ -17,7 +18,7 @@ contract TokenAddresses {
         return tokens[_tokenName];
     }
 
-    function addToken(string memory _tokenName, address _tokenAddress) external {
+    function addToken(string memory _tokenName, address _tokenAddress) external onlyOwner {
         require(tokens[_tokenName] == address(0), "Token already exists.");
         tokens[_tokenName] = _tokenAddress;
     }
