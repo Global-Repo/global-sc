c84d9b0e3c6383603bccf95f8e2f57ee5b9f85b7
BOJI <jordi.radev@gmail.com>
2021-10-14 03:54:06 +0200

    VSV-03 | Improper removal of user

--- contracts/Vaults/VaultStaked.sol
+++ contracts/Vaults/VaultStaked.sol
@@ -159,7 +159,13 @@ contract VaultStaked is IDistributable, ReentrancyGuard, RewarderRestriction {
     function _deleteUser(address _account) private {
         for (uint8 i = 0; i < users.length; i++) {
             if (users[i] == _account) {
-                delete users[i];
+                for (uint j = i; j<users.length-1; j++)
+                {
+                    users[j] = users[j+1];
+                }
+                users.pop();
+
+                //delete users[i];
             }
         }
     }
