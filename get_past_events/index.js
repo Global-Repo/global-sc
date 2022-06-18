674af665d5f5b615050747000cc0a74750179052
gyss <gravedian@gmail.com>
2022-06-18 23:11:00 +0100

    added basic scan events script

---
+++ get_past_events/index.js
@@ -0,0 +1,28 @@
+// const { web3 } = require("hardhat");
+const Web3 = require("web3");
+const abi = require("./abi");
+
+const Web3_bsc = new Web3("https://bsc-dataseed1.binance.org:443");
+
+const ADDRESS = "0x1dF0B759ee5b144852EEb571bbA9CCA2aec66b35";
+const FROM_BLOCK = "18610000";
+const TO_BLOCK = "18615000";
+
+const contract = new Web3_bsc.eth.Contract(abi, ADDRESS);
+
+contract
+  .getPastEvents(
+    "Swap", // "allEvents",
+    {
+      fromBlock: FROM_BLOCK,
+      toBlock: TO_BLOCK,
+    }
+  )
+  .then(function (events) {
+    console.log("___ OK", JSON.stringify(events));
+  })
+  .catch((err) => {
+    console.error("____ ERR", err);
+  });
+
+// https://bscscan.com/tx/0xcdceb49c13de0af901b73adacead01b8a877c00b44023459dbc6f8ce736c3a75
